df054d5aa34202a70226bc8bce30f846884f4b3f
Gabriel Martins <gabrielcust@gmail.com>
2023-07-05 16:53:18 -0300

    requisito 4 - Gabriel e Otc

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Search from './pages/Search';
+import Layout from './pages/Layout';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={ <Search /> } />
+      <Route path="/" element={ <Layout /> }>
+        <Route index element={ <Search /> } />
+      </Route>
     </Routes>
   );
 }
