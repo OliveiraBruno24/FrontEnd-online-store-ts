fcc2280af4216e93cd03aac789612e34be34aad7
Bruno de Oliveira <brunodeoliveira2000@hotmail.com>
2023-07-05 17:18:25 -0300

    requisito 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { Routes, Route } from 'react-router-dom';
-import React from 'react';
-import logo from './logo.svg';
+import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import Search from './pages/Search';
+import Cart from './pages/Cart';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={ <Search /> } />
+      <Route path="/cart" element={ <Cart /> } />
     </Routes>
   );
 }
