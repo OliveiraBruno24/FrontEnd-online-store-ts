9b71377b91f1ae56635f43991fd035434f599728
Gabriel Martins <gabrielcust@gmail.com>
2023-07-07 20:49:22 -0300

    finalização 10 + conflict

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Cart from '../components/Cart/Cart';
 import { ProductDetailsWithQuantity } from '../types/type';
